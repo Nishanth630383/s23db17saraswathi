397570ef02e5fab4c485c7de5fa44b5830a4cf44
Nishanth Raju <s559217@nwmissouri.edu>
2023-04-10 03:16:52 -0500

    made changes in controller

--- controllers/roadtransport.js
+++ controllers/roadtransport.js
@@ -15,14 +15,42 @@ exports.roadtransport_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: roadtransport detail: ' + req.params.id);
 };
 // Handle roadtransport create on POST.
-exports.roadtransport_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: roadtransport create POST');
-};
-// Handle roadtransport delete form on DELETE.
+exports.roadtransport_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new roadtransport();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"roadtransport_type":"goat", "cost":12, "size":"large"}
+    document.roadtransport_type = req.body.roadtransport_type;
+    document.roadtransport_name = req.body.roadtransport_name;
+    document.roadtransport_cost = req.body.roadtransport_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };// Handle roadtransport delete form on DELETE.
 exports.roadtransport_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: roadtransport delete DELETE ' + req.params.id);
 };
 // Handle roadtransport update form on PUT.
 exports.roadtransport_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: roadtransport update PUT' + req.params.id);
+};
+
+// VIEWS
+// Handle a show all view
+exports.roadtransport_view_all_Page = async function(req, res) {
+    try{
+    theroadtransport = await roadtransport.find();
+    res.render('roadtransport', { title: 'roadtransport Search Results', results: theroadtransport });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
 };
--- routes/roadtransport.js
+++ routes/roadtransport.js
@@ -1,7 +1,8 @@
 var express = require('express');
+const roadtransport_controlers= require('../controllers/roadtransport');
 var router = express.Router();
 
-class roadtransport{
+/*class roadtransport{
     constructor(roadtransport_type, roadtransport_name, roadtransport_cost){
         this.roadtransport_type=roadtransport_type;
         this.roadtransport_name=roadtransport_name;
@@ -9,12 +10,12 @@ class roadtransport{
     }
 }
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
     let r1= new roadtransport('2 wheeler','Bike',10000);
     let r2= new roadtransport('4 wheeler','Car',50000);
     let r3= new roadtransport('6 wheeler','Truck',100000);
   res.render('roadtransport', { title: 'Search Results Road Transport',roadtransport : [r1,r2,r3] });
-});
-
+});*/
+router.get('/', roadtransport_controlers.roadtransport_view_all_Page );
 module.exports = router;
